2d0a4af21823b706570e58ee05771e1b5c4cec52
Rafa Lima <rafalima@pop-os.localdomain>
2023-06-20 16:08:31 -0300

    feat: Add entities and controlles to states and cities

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,31 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'user' })
+export class UserEntity {
+  @PrimaryGeneratedColumn('rowid')
+  id: number;
+
+  @Column({ name: 'name', nullable: false })
+  name: string;
+
+  @Column({ name: 'email', nullable: false })
+  email: string;
+
+  @Column({ name: 'password', nullable: false })
+  password: string;
+
+  @Column({ name: 'cpf', nullable: false })
+  cpf: string;
+
+  @Column({ name: 'phone' })
+  phone: string;
+
+  @Column({ name: 'type_user' })
+  typeUser: number;
+
+  @Column({ name: 'created_at' })
+  createdAt: Date;
+
+  @Column({ name: 'updated_at' })
+  updatedAt: Date;
+}
